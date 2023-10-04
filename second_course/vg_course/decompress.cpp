7d967ea77a18341b97e3bf6dda1cc5294a36bcf2
YakuninVladimir <87427286+YakuninVladimir@users.noreply.github.com>
2023-10-04 20:21:22 +0300

    Add files via upload

--- second_course/vg_course/decompress.cpp
+++ second_course/vg_course/decompress.cpp
@@ -6,9 +6,9 @@
 
 using namespace std;
 
-char buf = 0, pos = 0;
 
 void wr(ofstream &out, int bit) {
+  static char buf = 0, pos = 0;
   if (pos >= 8){
     out.write(&buf, 1);
     pos = 0;
@@ -17,6 +17,16 @@ void wr(ofstream &out, int bit) {
   char mask = (char)(bit ? 1 : 0) << pos;
   buf += mask;
   pos++;
+}
+
+void rd(ifstream & inp, char & bit) {
+  static char buf, pos;
+  if (pos >= 8) {
+    inp.read(buf, 1);
+    pos = 0;
+  }
+  char mask = (char)(bit ? 1 : 0) << pos;
+  bit = (mask & buf) >> pos;
 }
 
 int main() {
@@ -29,4 +39,31 @@ int main() {
   for (auto & i : s)weights[i]++;
   vector<int>pref(257, 0);
   for (int i = 1; i <= 256; i++)pref[i] = weights[i - 1] + pref[i - 1];
+  unsigned short l = 0, h = 65535;
+  unsigned short q1 = ((int)h + 1)/4, q2 = q1 * 2, q3 = q1*3, bits = 0;
+  char value;
+  inp.read(value, 1);
+  while(inp.peek() != EOF) {
+    int ind = cur;
+    int prl = l, prh = h;
+    l = prl + (prh - prl + 1) * pref[ind] / del;
+    h = prl + (prh - prl + 1) * pref[ind + 1] / del - 1;
+    while (true) {
+      if (h < q2) {}
+      else if (l >= q2){
+        value -= q2;
+        l -= q2;
+        h -= q2;
+      }
+      else if (l >= q1 && h < q3) {
+        l -= q2;
+        h -= q2
+      } else break;
+      l += l;
+      h += h + 1;
+      char bit;
+      rd(inp, bit);
+      value = (value << 1) + bit;
+    }
+  }
 }
