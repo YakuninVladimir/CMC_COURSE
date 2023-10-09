2cb420dc8ea5966d06dce7dfe31436d1275b651b
YakuninVladimir <87427286+YakuninVladimir@users.noreply.github.com>
2023-10-10 02:08:57 +0300

    Add files via upload

--- second_course/vg_course/decompress.cpp
+++ second_course/vg_course/decompress.cpp
@@ -22,38 +22,33 @@ void wr(ofstream &out, int bit) {
 }
 
 unsigned char rd(ifstream &inp) {
-  static int pos = 0;
-  unsigned char *buf;
+  static int pos = 8;
+  static unsigned char buf;
   if (pos >= 8) {
-    inp.read((char *)buf, 1);
+    inp.read((char *)&buf, 1);
     pos = 0;
   }
-  unsigned char mask = 1 << (7 - pos);
+  unsigned char mask = 1 << (pos);
   pos++;
-  return (mask & *buf) >> (8 - pos);
+  cout << ((mask & buf) >> (pos - 1));
+  return (mask & buf) >> (pos - 1);
 }
 
 int get_symb(us value, us l, us h, vector<int> &pref) {
-  // cerr << "ffffuuu" << endl;
   int len = h - l + 1;
   int del = pref[256];
   for (int i = 1; i <= 256; i++) {
-    // cout << l + len * pref[i - 1] / del << ' ' << value << ' '
-    //    << l + len * pref[i] / del << endl;
     if (value >= l + len * pref[i - 1] / del &&
         value < l + len * pref[i] / del) {
       return i - 1;
     }
   }
-  // cerr << "lildeadxd" << endl;
 }
 
 int main() {
-  cout << "fuck" << endl;
   ifstream table("./test");
   ifstream inp("./out");
   ofstream out("./output");
-  cerr << "fuck" << endl;
   string s;
   getline(table, s);
   vector<int> weights(256);
@@ -66,56 +61,43 @@ int main() {
   }
   us l = 0, h = 65535;
   int del = pref[256];
-  for (auto i : pref) {
-    cout << i << " ";
-  }
-  us q1 = ((int)h + 1) / 4, q2 = q1 * 2, q3 = q1 * 3, bits = 0;
-  us value;
+  us q1 = ((int)h + 1) / 4, q2 = q1 * 2, q3 = q1 * 3;
+  us value = 0;
   inp.read((char *)&value, 2);
-  cout << "<<<" << bitset<16>(value) << ">>>" << endl;
   us m = 1, nn = 0;
   for (int i = 0; i < 16; i++) {
     nn <<= 1;
     nn ^= (m & value) >> i;
     m <<= 1;
-    cout << "$" << bitset<16>(nn) << "$" << endl;
   }
   value = nn;
-  cout << "<<<" << bitset<16>(value) << ">>>" << endl;
-  for (int e = 0; e < 12; e++) {
-    cout << '<' << l << ' ' << value << ' ' << h << '>' << endl;
+  for (int e = 0; e < 445; e++) {
     int ind = get_symb(value, l, h, pref);
-    cout << "F " << ind << endl;
     char sym = ind;
     out.write(&sym, 1);
     int prl = l, prh = h;
     l = prl + (prh - prl + 1) * pref[ind] / del;
     h = prl + (prh - prl + 1) * pref[ind + 1] / del - 1;
     while (true) {
-      cout << '$' << l << ' ' << value << ' ' << h << '$' << endl;
       if (h < q2) {
-        cout << "(1)" << endl;
+        cout << "";
       } else if (l >= q2) {
-        cout << "(2)" << endl;
+        cout << "";
         value -= q2;
         l -= q2;
         h -= q2;
       } else if (l >= q1 && h < q3) {
-        cout << "(3)" << endl;
+        cout << "";
         l -= q1;
         h -= q1;
         value -= q1;
       } else {
         break;
       }
-      // cout << "sex" << endl;
-      // cout << l << ' ' << h << ' ' << ind << endl;
-      // cout << pref[ind] << ' ' << pref[ind + 1] << endl;
-
       l += l;
       h += h + 1;
       unsigned char bit = rd(inp);
-      cout << (int)bit << endl;
+      cout << (unsigned)bit << endl;
       value = (value << 1) + bit;
     }
   }
