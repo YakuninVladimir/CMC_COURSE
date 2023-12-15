c0c5794c2ccc509727545365cfe35a957c3b4cb3
YakuninVladimir <87427286+YakuninVladimir@users.noreply.github.com>
2023-12-15 16:59:25 +0300

    Add files via upload

--- second_course/os/14/14_4.c
+++ second_course/os/14/14_4.c
@@ -12,6 +12,11 @@ enum
     SYS_BASE = 10,
 };
 
+pid_t cur_pid;
+int maxv;
+FILE *channel[2];
+volatile sig_atomic_t proc;
+
 int
 strtol_s(char *s)
 {
@@ -25,55 +30,40 @@ strtol_s(char *s)
 }
 
 void
-func1(FILE *rfd, FILE *wfd, int max_value)
+handler(int signal)
 {
-    printf("Gay1\n");
-    fflush(stdout);
-    pause();
-    pid_t pid;
-    fscanf(rfd, "%d", &pid);
-    printf("%d\n", pid);
-    kill(pid, SIGUSR1);
-    pause();
-    while (1) {
-        int x;
-        fscanf(rfd, "%d", &x);
-        if (x >= max_value - 1) {
-            _exit(0);
-        }
-        ++x;
-        printf("%d %d\n", 1, x);
-        fflush(stdout);
-        fprintf(wfd, "%d\n", x);
-        kill(pid, SIGUSR1);
-        pause();
+    int x;
+    fscanf(channel[0], "%d", &cur_pid);
+    fscanf(channel[0], "%d", &x);
+    x++;
+    fprintf(channel[1], "%d\n", getpid());
+    fprintf(channel[1], "%d\n", x);
+    if (x >= maxv) {
+        kill(cur_pid, SIGUSR1);
+        exit(0);
     }
+    fprintf(stdout, "%d %d\n", proc, x);
+    kill(cur_pid, SIGUSR1);
 }
 
 void
-func2(FILE *rfd, FILE *wfd, int max_value)
+func1()
 {
-    printf("Gay2\n");
-    fflush(stdout);
-    pause();
-    pid_t pid;
-    fscanf(rfd, "%d", &pid);
-    printf("%d\n", pid);
-    kill(pid, SIGUSR1);
+    proc = 1;
     while (1) {
-        int x;
         pause();
-        fscanf(rfd, "%d", &x);
-        kill(pid, SIGUSR1);
-        if (x >= max_value - 1) {
-            _exit(0);
-        }
-        ++x;
-        printf("%d %d\n", 2, x);
-        fflush(stdout);
-        fprintf(wfd, "%d\n", x);
-        kill(pid, SIGUSR1);
     }
+    exit(0);
+}
+
+void
+func2()
+{
+    proc = 2;
+    while (1) {
+        pause();
+    }
+    exit(0);
 }
 
 int
@@ -83,44 +73,47 @@ main(int argc, char *argv[])
         fprintf(stderr, "Error, wrong num of arguments\n");
         return 1;
     }
-    int maxv = strtol_s(argv[1]);
+    sigaction(SIGUSR1, &(struct sigaction){.sa_handler = handler, .sa_flags = SA_RESTART}, NULL);
+    maxv = strtol_s(argv[1]);
     int pip[2];
     pipe(pip);
-    FILE *channel[2];
     channel[0] = fdopen(pip[0], "r");
     channel[1] = fdopen(pip[1], "w");
 
     setbuf(channel[0], NULL);
     setbuf(channel[1], NULL);
-
+    setbuf(stdin, NULL);
+    setbuf(stdout, NULL);
 
     int pid1 = fork();
     if (!pid1) {
-        printf("?\n");
-        func1(channel[0], channel[1], maxv);
+        func1();
         _exit(1);
     }
     int pid2 = fork();
     if (!pid2) {
-        printf("?\n");
-        func2(channel[0], channel[1], maxv);
+        func2();
         _exit(1);
     }
-    // pid2 = fork();
-    // if (!pid2) {
-    //     func2(channel[0], channel[1], maxv);
-    //     _exit(1);
-    // }
 
     fprintf(channel[1], "%d\n", pid2);
-    fprintf(channel[1], "%d\n", pid1);
     fprintf(channel[1], "0\n");
     fclose(channel[0]);
     fclose(channel[1]);
-    kill(pid1, SIGUSR1);
-    // kill(pid2, SIGUSR1);
-    waitpid(pid1, NULL, 0);
-    waitpid(pid2, NULL, 0);
-    printf("Done\n");
-}
 
+    kill(pid1, SIGUSR1);
+
+    int status;
+    waitpid(pid1, &status, 0);
+    if (!(WIFEXITED(status) && !WEXITSTATUS(status))) {
+        fprintf(stderr, "Error, wring status of proccess finishing\n");
+        exit(1);
+    }
+    waitpid(pid2, &status, 0);
+    if (!(WIFEXITED(status) && !WEXITSTATUS(status))) {
+        fprintf(stderr, "Error, wring status of proccess finishing\n");
+        exit(1);
+    }
+    printf("Done\n");
+    return 0;
+}
